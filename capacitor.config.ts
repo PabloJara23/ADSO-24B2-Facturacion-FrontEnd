17b0d29251a7ec680a1ebda50f01ee00de1da202
Jorge Ballesteros <joba1021@netcracker.com>
2025-08-20 20:02:37 -0500

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ADSO-24B2-Facturacion-FrontEnd',
+  webDir: 'www'
+};
+
+export default config;
